d28edb7a479e424e3c7a350de757ad0733b83779
Oleg Kornitskiy <iam@kornitskiy.ru>
2026-05-13 10:37:25 +0500

    fix _qs warning

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
                  .arg(QCoreApplication::applicationVersion()));
 
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/main.qml"_qs);
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app, [url](QObject *obj, const QUrl &objUrl)
                      {
         if (!obj && url == objUrl)
